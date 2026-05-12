c61effda59a863609c8aba4c2a820cfca329b878
mairuu <kmr.mairuu@gmail.com>
2026-05-12 17:10:49 +0700

    format

--- src/routes/health/+server.ts
+++ src/routes/health/+server.ts
@@ -1,8 +1,8 @@
-import { json, type RequestHandler } from "@sveltejs/kit";
+import { json, type RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = () => {
-    return json({
-        status: "ok",
-        timestamp: new Date().toISOString()
-    });
-}
+	return json({
+		status: 'ok',
+		timestamp: new Date().toISOString()
+	});
+};
--- src/routes/metrics/+server.ts
+++ src/routes/metrics/+server.ts
@@ -2,10 +2,10 @@ import { register } from '$lib/metrics';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async () => {
-    const metrics = await register.metrics();
-    return new Response(metrics, {
-        headers: {
-            'Content-Type': register.contentType
-        }
-    });
+	const metrics = await register.metrics();
+	return new Response(metrics, {
+		headers: {
+			'Content-Type': register.contentType
+		}
+	});
 };
